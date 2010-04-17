037019881728d92ae43769459178a830145dc67e
Daniel DeCovnick <danielhd@mbp-2530.local>
2010-04-16 19:36:44 -0700

    initial Mac commit

---
+++ Mac/Handoff/main.m
@@ -0,0 +1,14 @@
+//
+//  main.m
+//  Handoff
+//
+//  Created by Daniel DeCovnick on 4/16/10.
+//  Copyright Softyards Software 2010. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+int main(int argc, char *argv[])
+{
+    return NSApplicationMain(argc,  (const char **) argv);
+}
