79c53c387ea32893183308cdc630e3c1b23b13b2
Chris Haseman <haseman@wanderingoak.net>
2010-04-17 17:44:59 -0700

    adding stuff for the HOItem and useless code to the myNetworkController

--- iPhone/Handoff/Classes/HOMyNetworkController.h
+++ iPhone/Handoff/Classes/HOMyNetworkController.h
@@ -9,10 +9,26 @@
 #import <Foundation/Foundation.h>
 #import "BLIP.h"
 
+@protocol HOConnectionListener
+-(void)gotDataPayload:(NSDictionary *)payload;
+@end
 
-
-@interface hoMyNetworkController : NSObject {
-	
+@interface HOMyNetworkController : NSObject <
+									TCPListenerDelegate,
+									BLIPConnectionDelegate>
+{
+	id delegate;
+	BLIPListener *_listener;
 }
+
+-(NSArray *)getAvailableDevices;
+-(void)sendString:(NSString*)data;
+-(void)sendData:(NSString*)data;
+-(id)init;
+-(void)processIncomingData: (NSData*)data: (NSString *)contentType;
+- (NSString *)processOutgoingData: (NSDictionary *)data content:(NSString *)contentType;
+-(NSObject *)getData:(NSString *)data: (NSString *)contentType;
+
+@property (nonatomic, assign) id delegate;
 
 @end
